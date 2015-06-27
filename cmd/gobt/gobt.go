4362193a6be0294a365959602c68a87f320d37db
Blazej Bucko <blazej.bucko@gmail.com>
2015-06-27 22:16:39 +0200

    Removed unneded code

--- cmd/gobt/gobt.go
+++ cmd/gobt/gobt.go
@@ -4,15 +4,8 @@ import (
 	"fmt"
 
 	"github.com/bbucko/gobt/torrent"
-	"github.com/marksamman/bencode"
 )
 
 func main() {
 	fmt.Println("Hello, 世界 {}", torrent.New(""))
-
-	dict := make(map[string]interface{})
-	dict["string key"] = "hello world"
-	dict["int key"] = 123456
-
-	fmt.Printf("bencode encoded dict: %s\n", bencode.Encode(dict))
 }
